4ced51ffc7add5df25b2916df7b48081f2f03cde
IlyasSultanov <sultanvoilias80@gmail.com>
2025-11-21 18:19:02 +0500

    fead: base exceptions

---
+++ app/exceptions/base.py
@@ -0,0 +1,33 @@
+from typing import Any, Optional, Dict
+
+
+class ExceptionBase(Exception):
+    """Base Exception"""
+
+    def __init__(self, message: str, context: Optional[Dict[str, Any]] = None) -> None:
+        self.message = message
+        self.context = context or {}
+        super().__init__(message)
+
+
+class AuthException(ExceptionBase):
+    """Auth Exception"""
+
+    def __init__(
+        self, message: str = "Access denied", context: Optional[Dict[str, Any]] = None
+    ) -> None:
+        super().__init__(f"Unauthorized: {message}", context)
+
+
+class NotFoundException(ExceptionBase):
+    """Not Found Exception"""
+
+    def __init__(self, message: str, context: Optional[Dict[str, Any]] = None) -> None:
+        super().__init__(f"Not Found: {message}", context)
+
+
+class ValidationException(ExceptionBase):
+    """Validation Exception"""
+
+    def __init__(self, message: str, context: Optional[Dict[str, Any]] = None) -> None:
+        super().__init__(f"Validation Error: {message}", context)
