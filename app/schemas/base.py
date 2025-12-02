de5ab89d1f6be41801d28070e4fecc1b900434c9
IlyasSultanov <sultanvoilias80@gmail.com>
2025-12-02 23:12:00 +0500

    fix: update base schemas and added default time zone

--- app/schemas/base.py
+++ app/schemas/base.py
@@ -1,11 +1,23 @@
 from datetime import datetime
+from typing import Optional
 from uuid import UUID
 
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, ConfigDict
+
+from app.schemas.mid import time_now
 
 
-class Base(BaseModel):
+class BaseSchemas(BaseModel):
+    model_config = ConfigDict(
+        populate_by_name=True,
+        from_attributes=True,
+        json_schema_extra={"example": {"id": "550e8400-e29b-41d4-a716-446655440000"}},
+    )
+
+
+class Base(BaseSchemas):
+
     id: UUID = Field(...)
-    created_at: datetime = Field(...)
-    updated_at: datetime = Field(...)
-    deleted_at: datetime = Field(...)
+    created_at: datetime = Field(default_factory=time_now)
+    updated_at: datetime = Field(default_factory=time_now)
+    deleted_at: Optional[datetime] = Field(default=None)
