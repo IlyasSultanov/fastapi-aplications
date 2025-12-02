bfc65fb8720a83b815c8513b6b3155396a49bc6e
IlyasSultanov <sultanvoilias80@gmail.com>
2025-12-02 23:10:46 +0500

    fead: base crude

--- app/crud/base.py
+++ app/crud/base.py
@@ -1,16 +1,16 @@
 from typing import Generic, TypeVar, Type, Optional, List, Dict, Any, Union
-from pydantic import BaseModel
 from uuid import UUID
 
-from sqlalchemy.ext.asyncio import AsyncSession
+from pydantic import BaseModel
 from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
 
-from app.database.base_class import BaseModel as DBBaseModls
-from app.exceptions.base import NotFoundException
+from app.database.base_class import BaseModel as DBBaseModel
 
-ModelType = TypeVar("ModelType", bound="DBBaseModls")
-CreateSchemas = TypeVar("CreateSchemas", bound="BaseModel")
-UpdateSchemas = TypeVar("UpdateSchemas", bound="BaseModel")
+ModelType = TypeVar("ModelType", bound=DBBaseModel)
+CreateSchemas = TypeVar("CreateSchemas", bound=BaseModel)
+UpdateSchemas = TypeVar("UpdateSchemas", bound=BaseModel)
+
 
 class BaseCrud(Generic[ModelType, CreateSchemas, UpdateSchemas]):
 
@@ -18,10 +18,8 @@ class BaseCrud(Generic[ModelType, CreateSchemas, UpdateSchemas]):
         self.model = model
         self.db = db
 
-
     async def get_by_id(
-            self, id: UUID,
-            include_deleted: bool = False
+        self, id: UUID, include_deleted: bool = False
     ) -> Optional[ModelType]:
         query = select(self.model).where(self.model.id == id)
 
@@ -29,17 +27,17 @@ class BaseCrud(Generic[ModelType, CreateSchemas, UpdateSchemas]):
             query = query.where(self.model.deleted_at.is_(None))
 
         result = await self.db.execute(query)
-        
+
         return result.scalar_one_or_none()
-    
+
     async def get_multi_data(
-            self,
-            skip: int = 0,
-            limit: int = 100,
-            include_deleted: bool = False,
-            filters: Optional[Dict[str, Any]] = None
+        self,
+        skip: int = 0,
+        limit: int = 100,
+        include_deleted: bool = False,
+        filters: Optional[Dict[str, Any]] = None,
     ) -> List[ModelType]:
-        
+
         query = select(self.model)
 
         if not include_deleted:
@@ -55,18 +53,15 @@ class BaseCrud(Generic[ModelType, CreateSchemas, UpdateSchemas]):
         result = await self.db.execute(query)
 
         return list(result.scalars().all())
-    
-    async def create(
-            self,
-            obj_in: Union[CreateSchemas, Dict[str, Any]]
-    ) -> ModelType:
-        
+
+    async def create(self, obj_in: Union[CreateSchemas, Dict[str, Any]]) -> ModelType:
+
         if hasattr(obj_in, "model_dump"):
-            obj_data = obj_in.model_dump()
+            obj_data = obj_in.model_dump()  # type: ignore
         else:
             obj_data = obj_in
 
-        db_obj = self.model(**obj_data)
+        db_obj = self.model(**obj_data)  # type: ignore
 
         self.db.add(db_obj)
 
@@ -74,29 +69,25 @@ class BaseCrud(Generic[ModelType, CreateSchemas, UpdateSchemas]):
         await self.db.refresh(db_obj)
 
         return db_obj
-    
+
     async def update(
-            self,
-            id: UUID,
-            obj_in: Union[UpdateSchemas, Dict[str, Any]]
+        self, id: UUID, obj_in: Union[UpdateSchemas, Dict[str, Any]]
     ) -> Optional[ModelType]:
-        
+
         db_obj = await self.get_by_id(id)
         if not db_obj:
-            return None # TODO i need added some exceptions for return data
-        
+            return None  # TODO i need added some exceptions for return data
+
         if hasattr(obj_in, "model_dump"):
-            update_data = await obj_in.model_dump(exclude_unset=True)
+            update_data = obj_in.model_dump(exclude_unset=True)  # type: ignore[arg-type]
         else:
             update_data = obj_in
 
-            for field, value in update_data.items():
-                if hasattr(db_obj, field):
-                    getattr(db_obj, field, value)
+        for field, value in update_data.items():  # type: ignore[union-attr]
+            if hasattr(db_obj, field):
+                setattr(db_obj, field, value)
 
         await self.db.commit()
         await self.db.refresh(db_obj)
 
         return db_obj
-
-
